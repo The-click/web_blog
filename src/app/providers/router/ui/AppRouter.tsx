feefba2b09dda0933992be90891e03f2f6a3cce0
The-click <0leg.19-94@mail.ru>
2023-02-11 17:19:06 +0300

    add not found page, add loader page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,21 @@
-import React, { Suspense } from 'react'
-import PropTypes from 'prop-types'
-import { Route, Routes } from 'react-router-dom'
-import { MainPage } from 'pages/Main'
-import { AboutPage } from 'pages/About'
-import { routeConfig } from 'shared/config/routerConfig/routeConfig'
+import React, { Suspense } from "react";
+import PropTypes from "prop-types";
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "pages/Main";
+import { AboutPage } from "pages/About";
+import { routeConfig } from "shared/config/routerConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader";
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className='page-wrapper'>
-    <Routes>
-       {Object.values(routeConfig).map(({path, element}) => <Route path={path} element={element} />)}
-    </Routes> 
-    </div>
+    <Suspense fallback={<PageLoader />}>
+        <div className="page-wrapper">
+            <Routes>
+                {Object.values(routeConfig).map(({ path, element }) => (
+                    <Route path={path} element={element} />
+                ))}
+            </Routes>
+        </div>
     </Suspense>
-  )
+);
 
-
-export default AppRouter
+export default AppRouter;
--- src/shared/config/routerConfig/routeConfig.tsx
+++ src/shared/config/routerConfig/routeConfig.tsx
@@ -1,24 +1,31 @@
-import { AboutPage } from "pages/About"
-import { MainPage } from "pages/Main"
-import {  RouteProps } from "react-router-dom"
+import { AboutPage } from "pages/About";
+import { MainPage } from "pages/Main";
+import { NotFound } from "pages/NotFound";
+import { RouteProps } from "react-router-dom";
 
-export enum AppRoutes{
-    MAIN='main',
-    ABOUT='about',
+export enum AppRoutes {
+    MAIN = "main",
+    ABOUT = "about",
+    NOTFOUND = "not_found",
 }
 
-export const RoutePath: Record<AppRoutes, string> ={
-    [AppRoutes.ABOUT]: '/about',
-    [AppRoutes.MAIN]:'/'
-}
+export const RoutePath: Record<AppRoutes, string> = {
+    [AppRoutes.ABOUT]: "/about",
+    [AppRoutes.MAIN]: "/",
+    [AppRoutes.NOTFOUND]: "*",
+};
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
-        path:RoutePath.main,
-        element: <MainPage />
+        path: RoutePath.main,
+        element: <MainPage />,
     },
-    [AppRoutes.ABOUT]:{
-        path:RoutePath.about,
-        element: <AboutPage />
-    }
-}
+    [AppRoutes.ABOUT]: {
+        path: RoutePath.about,
+        element: <AboutPage />,
+    },
+    [AppRoutes.NOTFOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFound />,
+    },
+};
