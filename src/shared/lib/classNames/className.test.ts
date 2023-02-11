47f644fea6cd17e2befe0c1b1a09bc6933ec1cba
The-click <0leg.19-94@mail.ru>
2023-02-11 15:55:30 +0300

    add jest, add first test

---
+++ src/shared/lib/classNames/className.test.ts
@@ -0,0 +1,29 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with only first param", () => {
+    expect(classNames("className")).toBe("className");
+  });
+  test("with additional params", () => {
+    const excepted = "className red blue";
+    expect(classNames("className", {}, ["red", "blue"])).toBe(excepted);
+  });
+  test("with mods", () => {
+    const excepted = "className red blue scrollable lazy";
+    expect(classNames("className", { scrollable: true, lazy: true }, ["red", "blue"])).toBe(
+      excepted
+    );
+  });
+  test("with mod false", () => {
+    const excepted = "className red blue scrollable";
+    expect(classNames("className", { scrollable: true, lazy: false }, ["red", "blue"])).toBe(
+      excepted
+    );
+  });
+  test("with mod undefined", () => {
+    const excepted = "className red blue scrollable lazy";
+    expect(classNames("className", { scrollable: true, lazy: true }, ["red", "blue"])).toBe(
+      excepted
+    );
+  });
+});
